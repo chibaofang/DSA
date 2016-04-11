cc39a8e7bfb4d937b6b83cebb4c97535d0a2bc27
chibaofang <13474379185@163.com>
2016-04-11 18:08:36 +0800

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string user_name;
+    cout<<"PLEASE ENTER THE NAME:";
+    cin>>user_name;
+    cout<<user_name;
+    return 0;
+}
